0ffa60b3cdfcf22aa31a088159794257ab6e961b
Muhammad Rizqi Ghana <mrizqighana@outlook.co.id>
2024-10-19 14:38:02 +0700

    Nambahin halaman homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontpageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontpageController::class, 'index'])->name('frontpage.index');
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,27 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+                sora: ['Sora', 'sans-serif']
             },
+            backgroundImage: {
+                'hero-bg': "url('../img/Screenshot 2024-10-17 204712.png')",
+            },
+            colors: {
+                'primary': {
+                    100: '#eadfd6',
+                    200: '#d5bfad',
+                    300: '#c1a085',
+                    400: '#ac805c',
+                    500: '#976033',
+                    600: '#88562e',
+                    700: '#6a4324',
+                    800: '#4c301a',
+                    900: '#2d1d0f',
+                },
+            }
         },
+       
     },
     plugins: [],
 };
