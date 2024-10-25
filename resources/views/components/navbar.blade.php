289c3b4c8da1426b6b4811c5562521ca3a604767
SayidanNazmyd <nazmydsayidan@gmail.com>
2024-10-25 13:54:49 +0700

    ini dah

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,13 @@
 <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
     <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
-        <div class="hidden lg:flex items-center gap-3">
-            @yield('breadcrumb')
-        </div>
-        <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto gap-5">
-            <div class="flex items-center gap-1 md:ml-auto md:pr-4 bg-primary-100 text-primary-700 p-2.5 rounded-lg text-sm">
-                <span class="material-symbols-rounded">event</span>{{date('l, d M Y')}}
+        <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
+            <div class="flex items-center md:ml-auto md:pr-4">
+                <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
+                <span class="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
+                    <i class="fas fa-search"></i>
+                </span>
+                <input type="text" class="pl-9 text-sm focus:shadow-primary-outline ease w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none focus:transition-shadow" placeholder="Type here..." />
+                </div>
             </div>
             <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
                 <!-- online builder btn  -->
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'My Account')
 
-@section('breadcrumbs')
+@section('breadcrumb')
     <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
             <li class="text-sm leading-normal">
             <a class="text-white opacity-90" href="javascript:;">Dashboard</a>
