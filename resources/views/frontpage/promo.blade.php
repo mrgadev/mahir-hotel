439d7e34f81a2276b9e88024526a7f02082ffe83
SayidanNazmyd <nazmydsayidan@gmail.com>
2024-11-04 20:59:21 +0700

    Revisi Promo Yagesya

--- resources/views/frontpage/promo.blade.php
+++ resources/views/frontpage/promo.blade.php
@@ -83,7 +83,7 @@
     </div> --}}
     <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         @foreach ($promos as $promo)    
-        <div class="flex flex-col gap-5">
+        <div class="flex flex-col gap-5 mb-10">
             <div class="relative">
                 <img src="{{Storage::url($promo->cover)}}" alt="" class="w-full h-48 object-cover rounded-xl relative">
                 <div id="copyPromoCode" class="absolute bottom-5 left-5 flex items-center gap-1 px-3 py-1 rounded-full bg-primary-100 text-primary-600 w-fit text-sm">
